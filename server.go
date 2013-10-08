b4a89ae2a4a475d1daa458e7313bd93ffbf65efb
Alex Zelenskiy <zelenskiy.alex@gmail.com>
2013-10-08 11:31:47 -0500

    more working around heroku

--- server.go
+++ server.go
@@ -11,21 +11,31 @@ import (
 var start = time.Now()
 var lastRequests = make(map[string]time.Time)
 
+func getUserIp(req *http.Request) string {
+	//deal with heroku being weird
+	forwardedIp := req.Header.Get("X-Forwarded-For")
+	if forwardedIp == "" {
+		return req.RemoteAddr
+	} else {
+		return forwardedIp
+	}
+}
+
 func calculateTemp() float64 {
 	//Sin wave of temperatures
 	return math.Ceil(math.Sin(time.Since(start).Seconds()/100) * 100)
 }
 
 func getTemp(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, req.RemoteAddr)
-	if _, ok := lastRequests[req.RemoteAddr]; !ok ||
-		time.Since(lastRequests[req.RemoteAddr]).Seconds() > 2 {
-		fmt.Fprintln(res, time.Since(lastRequests[req.RemoteAddr]).Seconds())
-		lastRequests[req.RemoteAddr] = time.Now()
+	ip := getUserIp(req)
+	if _, ok := lastRequests[ip]; !ok ||
+		time.Since(lastRequests[ip]).Seconds() > 2 {
+		fmt.Fprintln(res, time.Since(lastRequests[ip]).Seconds())
+		lastRequests[ip] = time.Now()
 		fmt.Fprintln(res, calculateTemp())
 
 	} else {
-		lastRequests[req.RemoteAddr] = time.Now()
+		lastRequests[ip] = time.Now()
 		fmt.Fprintln(res, "you lose")
 	}
 
