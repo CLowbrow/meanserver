b68a735a33f148f85e4cd2d433ac8cc19f23a99b
Alex Zelenskiy <zelenskiy.alex@gmail.com>
2013-10-08 14:23:41 -0500

    making the server meaner

--- server.go
+++ server.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
 )
 
+type Weather struct {
+	Temperature float64
+	Conditions  string
+}
+
 var start = time.Now()
 var lastRequests = make(map[string]time.Time)
+var conditions = [...]string{"cloudy", "sunny", "foggy"}
 
 func getUserIp(req *http.Request) string {
-	//deal with heroku being weird
+	//normalize for running locally or on heroku
 	forwardedIp := req.Header.Get("X-Forwarded-For")
 	if forwardedIp == "" {
 		return req.RemoteAddr
@@ -23,22 +31,45 @@ func getUserIp(req *http.Request) string {
 
 func calculateTemp() float64 {
 	//Sin wave of temperatures
-	return math.Ceil(math.Sin(time.Since(start).Seconds()/100) * 100)
+	return math.Ceil(math.Sin(time.Since(start).Seconds()/300) * 100)
+}
+
+func getWeather() Weather {
+	return Weather{calculateTemp(), conditions[rand.Intn(3)]}
+}
+
+func generateRes(res http.ResponseWriter) {
+	// Randomly give back a good response or random garbage :)
+	dice := rand.Intn(6)
+
+	switch dice {
+	default:
+		res.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(getWeather())
+		fmt.Fprintln(res, string(response))
+	case 1:
+		res.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(res, "{ weather: malformed json here ++_--_(*&^$#$^&*(")
+	case 2:
+		res.WriteHeader(http.StatusTeapot)
+		fmt.Fprintln(res, "I'm A Teapot!")
+	}
+
 }
 
 func getTemp(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Access-Control-Allow-Origin", "*")
 	ip := getUserIp(req)
+	//Reject if this IP has made a request in the last two seconds
 	if _, ok := lastRequests[ip]; !ok ||
 		time.Since(lastRequests[ip]).Seconds() > 2 {
-		fmt.Fprintln(res, time.Since(lastRequests[ip]).Seconds())
 		lastRequests[ip] = time.Now()
-		fmt.Fprintln(res, calculateTemp())
-
+		generateRes(res)
 	} else {
 		lastRequests[ip] = time.Now()
-		fmt.Fprintln(res, "you lose")
+		res.WriteHeader(429)
+		fmt.Fprintln(res, "Too many requests")
 	}
-
 }
 
 func main() {
