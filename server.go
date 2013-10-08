d4137693b573b3dbcb0d6e97f565bc5db4c5978a
Alex Zelenskiy <zelenskiy.alex@gmail.com>
2013-10-08 00:40:31 -0500

    rate throttling

--- server.go
+++ server.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
+var start = time.Now()
+var lastRequests = make(map[string]time.Time)
+
+func calculateTemp() float64 {
+	//Sin wave of temperatures
+	return math.Ceil(math.Sin(time.Since(start).Seconds()/100) * 100)
+}
+
+func getTemp(res http.ResponseWriter, req *http.Request) {
+	ip := strings.Split(req.RemoteAddr, ":")[0]
+	if time.Since(lastRequests[ip]).Seconds() > 1.5 {
+		fmt.Fprintln(res, calculateTemp())
+	} else {
+		fmt.Fprintln(res, "you lose")
+	}
+	lastRequests[ip] = time.Now()
+}
+
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", getTemp)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, world")
 }
